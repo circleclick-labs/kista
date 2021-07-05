2420f2f808f7894721cbaffa228fda777d454cb9
Joel Ward <jmward@gmail.com>
2021-07-04 19:09:58 -0500

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='kista',
+      version='1.0.0',
+      description='miscellaneous stuff, old norse for bag',
+      url='https://github.com/circleclick-labs/kista.git',
+      author='Joel Ward',
+      author_email='jmward+python@gmail.com',
+      license='MIT',
+      packages=['kista'],
+      zip_safe=False)
