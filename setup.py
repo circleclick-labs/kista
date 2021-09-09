cef30dea23d01a0c0af7d6789ab1a4afd0ac8213
Joel Ward <jmward@gmail.com>
2021-09-08 20:19:32 -0500

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from setuptools import setup
 from kista import __name__, version
-requirements=[x.strip()for x in open('requirements.txt').readlines()if x!='\n']
+#requirements=[x.strip()for x in open('requirements.txt').readlines()if x!='\n']
 long_description=(Path(__file__).parent / "README.md").read_text()
 entry_points=dict(console_scripts=[f'{__name__}={__name__}.__main__'])
 setup(name=__name__,
@@ -15,7 +15,7 @@ setup(name=__name__,
       license='MIT',
       packages=[__name__],
       entry_points=entry_points,
-      install_requires=requirements,
+      install_requires=['web3', 'docopt'],
       classifiers=[
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: MIT License",
